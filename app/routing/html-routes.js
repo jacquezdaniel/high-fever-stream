d1df9f70fae5835444cce3738051711c17094731
Danny <jacquez.j.daniel@gmail.com>
2020-04-04 00:38:13 -0700

    routing

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -5,11 +5,11 @@ var path = require("path");
 module.exports = function (app) {
   // // Basic route that sends the userto the home page
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // If no matching route is found default to home
   app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/home.html"));
+    res.sendFile(path.join(__dirname + "../public/home.html"));
   });
 };
