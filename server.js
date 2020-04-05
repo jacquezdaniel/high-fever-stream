9110d56e2e7f5ff7dd6f655a1b7b3673e81dd97d
Danny <jacquez.j.daniel@gmail.com>
2020-04-04 19:46:25 -0700

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-const PORT = process.env.PORT || 3001;
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mighty-gorge-82269.herokuapp.com/",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
