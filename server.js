19b34665a1e8ab547b603c815aa76f4b2aa439ff
Danny <jacquez.j.daniel@gmail.com>
2020-04-04 17:35:27 -0700

    cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
