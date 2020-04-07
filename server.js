fd11d4edb10527c393f4168d11f9b2fde7e1c943
Danny <jacquez.j.daniel@gmail.com>
2020-04-07 12:07:10 -0700

    server

--- server.js
+++ server.js
@@ -2,26 +2,10 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const allowedOrigins = [
-  "http://localhost:8000",
-  "https://mighty-gorge-82269.herokuapp.com/",
-];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+io.on("connection", () => {});
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
@@ -32,6 +16,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 require("./routing/htmlRoutes")(app);
 
-app.listen(PORT, function () {
-  console.log("High Fever Stream listening on PORT " + PORT);
+server.listen(PORT, () => {
+  console.log(`High Fever is listening on port ${PORT}`);
 });
